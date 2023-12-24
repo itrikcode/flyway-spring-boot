6840d33133d75c0fc030ef720dd1a896856a778c
itrikcode <kumarviveklohaman@gmail.com>
2023-12-24 23:15:27 +0530

    fist commit for fly way testing

---
+++ src/test/java/com/itsp/flyway/FlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsp.flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
